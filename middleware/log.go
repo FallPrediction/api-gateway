884a5b028f482ed81e0b8ba15a178f55dc59d6db
ElizabethHuang <lizne6z0@gmail.com>
2025-09-28 21:39:22 +0800

    feat: Add response log; And add more info in request log.

--- middleware/log.go
+++ middleware/log.go
@@ -1,7 +1,10 @@
 package middleware
 
 import (
+	"api-gateway/helper"
 	"api-gateway/logger"
+	"io"
+	"math/rand/v2"
 	"net/http"
 
 	"go.uber.org/zap"
@@ -13,17 +16,43 @@ type Log struct {
 	baseMiddleware
 }
 
+func (m *Log) getTraceId() string {
+	resultBytes := [5]byte{}
+	character := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	for i := range len(resultBytes) {
+		resultBytes[i] = character[rand.IntN(len(character))]
+	}
+	return string(resultBytes[:])
+}
+
 func (m *Log) Handle() http.Handler {
 	logger := logger.NewLogger()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rw := helper.NewResponseWriter(w)
+		body, _ := io.ReadAll(r.Body)
+		traceId := m.getTraceId()
 		logger.Info(
 			"",
+			zap.String("trace_id", traceId),
 			zap.String("type", "request"),
 			zap.String("method", r.Method),
 			zap.String("uri", r.URL.Path),
 			zap.String("host", r.Host),
+			zap.Object("header", (*helper.ZapHeader)(&r.Header)),
+			zap.String("body", string(body)),
 		)
-		m.next.ServeHTTP(w, r)
+		m.next.ServeHTTP(rw, r)
+		logger.Info(
+			"",
+			zap.String("trace_id", traceId),
+			zap.String("type", "response"),
+			zap.String("method", r.Method),
+			zap.String("uri", r.URL.Path),
+			zap.String("host", r.Host),
+			zap.Object("header", (*helper.ZapHeader)(&r.Header)),
+			zap.String("body", string(rw.Body)),
+			zap.Int("status_code", rw.StatusCode),
+		)
 	})
 }
 
